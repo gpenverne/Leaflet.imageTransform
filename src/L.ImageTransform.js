612b5e31cee9cad200b800782bc233c784d76931
OriginalSin <saleks@scanex.ru>
2015-02-15 00:05:58 +0300

    Use eslint

--- src/L.ImageTransform.js
+++ src/L.ImageTransform.js
@@ -11,41 +11,41 @@ L.ImageTransform = L.ImageOverlay.extend({
             var yx = anchors[i];
             this._anchors.push(L.latLng(yx));
         }
-        
+
         if (this._map) {
             this._reset();
         }
     },
-    
+
     _latLngToLayerPoint: function(latlng) {
         return this._map.project(latlng)._subtract(this._map.getPixelOrigin());
     },
-    
+
     setClip: function(clipLatLngs) {
         var topLeft = this._latLngToLayerPoint(this._bounds.getNorthWest()),
             pixelClipPoints = [];
-        
+
         this.options.clip = clipLatLngs;
-        
+
         for (var p = 0; p < clipLatLngs.length; p++) {
             var mercPoint = this._latLngToLayerPoint(clipLatLngs[p]),
                 pixel = L.ImageTransform.Utils.project(this._matrix3d_inverse, mercPoint.x - topLeft.x, mercPoint.y - topLeft.y);
             pixelClipPoints.push(L.point(pixel[0], pixel[1]));
         }
-        
+
         this.setClipPixels(pixelClipPoints);
     },
-    
+
     setClipPixels: function(pixelClipPoints) {
         this._pixelClipPoints = pixelClipPoints;
         this._drawCanvas();
     },
-    
+
     setUrl: function (url) {
         this._url = url;
         this._imgNode.src = this._url;
     },
-    
+
     getClip: function() {
         return this.options.clip;
     },
@@ -67,7 +67,7 @@ L.ImageTransform = L.ImageOverlay.extend({
         } else {
             this._image.appendChild(this._imgNode);
             this._imgNode.style[L.DomUtil.TRANSFORM_ORIGIN] = '0 0';
-            
+
             // Hide imgNode until image has loaded
             this._imgNode.style.display = 'none';
         }
@@ -103,19 +103,19 @@ L.ImageTransform = L.ImageOverlay.extend({
     },
 
     _reset: function () {
-        if (this.options.clip && !this._imgNode.complete) return;
+        if (this.options.clip && !this._imgNode.complete) {return;}
         var div = this._image,
             imgNode = this.options.clip ? this._canvas : this._imgNode,
             topLeft = this._latLngToLayerPoint(this._bounds.getNorthWest()),
             size = this._latLngToLayerPoint(this._bounds.getSouthEast())._subtract(topLeft),
             anchors = this._anchors,
-            w = this._imgNode.width, 
+            w = this._imgNode.width,
             h = this._imgNode.height,
             pixels = [],
-            i;
+            i, len, p;
 
-        for (var i = 0, len = anchors.length; i < len; i++) {
-            var p = this._latLngToLayerPoint(anchors[i]);
+        for (i = 0, len = anchors.length; i < len; i++) {
+            p = this._latLngToLayerPoint(anchors[i]);
             pixels.push(L.point(p.x - topLeft.x, p.y - topLeft.y));
         }
 
@@ -123,29 +123,31 @@ L.ImageTransform = L.ImageOverlay.extend({
 
         div.style.width  = size.x + 'px';
         div.style.height = size.y + 'px';
-        
+
         var matrix3d = this._matrix3d = L.ImageTransform.Utils.general2DProjection(
             0, 0, pixels[0].x, pixels[0].y,
             w, 0, pixels[1].x, pixels[1].y,
             w, h, pixels[2].x, pixels[2].y,
             0, h, pixels[3].x, pixels[3].y
         );
-        
+
         //something went wrong (for example, target image size is less then one pixel)
         if (!matrix3d[8]) {
             return;
         }
-        
+
         //matrix normalization
-        for(i = 0; i != 9; ++i) matrix3d[i] = matrix3d[i]/matrix3d[8];
-        
+        for (i = 0; i !== 9; ++i) {
+            matrix3d[i] = matrix3d[i] / matrix3d[8];
+        }
+
         this._matrix3d_inverse = L.ImageTransform.Utils.adj(matrix3d);
-        
+
         imgNode.style[L.DomUtil.TRANSFORM] = this._getMatrix3dCSS(this._matrix3d);
         if (this.options.clip) {
             if (this._pixelClipPoints) {
                 this.options.clip = [];
-                for (var p = 0; p < this._pixelClipPoints.length; p++) {
+                for (p = 0; p < this._pixelClipPoints.length; p++) {
                     var mercPoint = L.ImageTransform.Utils.project(matrix3d, this._pixelClipPoints[p].x, this._pixelClipPoints[p].y);
                     this.options.clip.push(this._map.layerPointToLatLng(L.point(mercPoint[0] + topLeft.x, mercPoint[1] + topLeft.y)));
                 }
@@ -159,10 +161,10 @@ L.ImageTransform = L.ImageOverlay.extend({
 
     _getMatrix3dCSS: function(arr)	{		// get CSS atribute matrix3d
         var css = 'matrix3d(';
-        css += arr[0].toFixed(9) + "," + arr[3].toFixed(9) + ", 0," + arr[6].toFixed(9);
-        css += "," + arr[1].toFixed(9) + "," + arr[4].toFixed(9) + ", 0," + arr[7].toFixed(9);
-        css += ",0, 0, 1, 0";
-        css += "," + arr[2].toFixed(9) + "," + arr[5].toFixed(9) + ", 0, " + arr[8].toFixed(9) + ")";
+        css += arr[0].toFixed(9) + ',' + arr[3].toFixed(9) + ', 0,' + arr[6].toFixed(9);
+        css += ',' + arr[1].toFixed(9) + ',' + arr[4].toFixed(9) + ', 0,' + arr[7].toFixed(9);
+        css += ',0, 0, 1, 0';
+        css += ',' + arr[2].toFixed(9) + ',' + arr[5].toFixed(9) + ', 0, ' + arr[8].toFixed(9) + ')';
         return css;
     },
 
@@ -171,12 +173,12 @@ L.ImageTransform = L.ImageOverlay.extend({
             ctx = canvas.getContext('2d');
 
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.fillStyle = ctx.createPattern(this._imgNode, "no-repeat");
+        ctx.fillStyle = ctx.createPattern(this._imgNode, 'no-repeat');
 
         ctx.beginPath();
         for (var i = 0, len = this._pixelClipPoints.length; i < len; i++) {
             var pix = this._pixelClipPoints[i];
-            ctx[i ? 'lineTo' : 'moveTo'](pix.x, pix.y)
+            ctx[i ? 'lineTo' : 'moveTo'](pix.x, pix.y);
         }
         ctx.closePath();
         ctx.fill();
@@ -191,24 +193,24 @@ L.DomUtil.TRANSFORM_ORIGIN = L.DomUtil.testProp(
         ['transformOrigin', 'WebkitTransformOrigin', 'OTransformOrigin', 'MozTransformOrigin', 'msTransformOrigin']);
 
 //Based on http://math.stackexchange.com/questions/296794/finding-the-transform-matrix-from-4-projected-points-with-javascript
-!function() {
+(function() {
     function adj(m) { // Compute the adjugate of m
         return [
-            m[4]*m[8]-m[5]*m[7], m[2]*m[7]-m[1]*m[8], m[1]*m[5]-m[2]*m[4],
-            m[5]*m[6]-m[3]*m[8], m[0]*m[8]-m[2]*m[6], m[2]*m[3]-m[0]*m[5],
-            m[3]*m[7]-m[4]*m[6], m[1]*m[6]-m[0]*m[7], m[0]*m[4]-m[1]*m[3]
+            m[4] * m[8] - m[5] * m[7], m[2] * m[7] - m[1] * m[8], m[1] * m[5] - m[2] * m[4],
+            m[5] * m[6] - m[3] * m[8], m[0] * m[8] - m[2] * m[6], m[2] * m[3] - m[0] * m[5],
+            m[3] * m[7] - m[4] * m[6], m[1] * m[6] - m[0] * m[7], m[0] * m[4] - m[1] * m[3]
         ];
     }
 
     function multmm(a, b) { // multiply two matrices
         var c = Array(9);
-        for (var i = 0; i != 3; ++i) {
-            for (var j = 0; j != 3; ++j) {
+        for (var i = 0; i !== 3; ++i) {
+            for (var j = 0; j !== 3; ++j) {
                 var cij = 0;
-                for (var k = 0; k != 3; ++k) {
-                    cij += a[3*i + k] * b[3*k + j];
+                for (var k = 0; k !== 3; ++k) {
+                    cij += a[3 * i + k] * b[3 * k + j];
                 }
-                c[3*i + j] = cij;
+                c[3 * i + j] = cij;
             }
         }
         return c;
@@ -216,9 +218,9 @@ L.DomUtil.TRANSFORM_ORIGIN = L.DomUtil.testProp(
 
     function multmv(m, v) { // multiply matrix and vector
         return [
-            m[0]*v[0] + m[1]*v[1] + m[2]*v[2],
-            m[3]*v[0] + m[4]*v[1] + m[5]*v[2],
-            m[6]*v[0] + m[7]*v[1] + m[8]*v[2]
+            m[0] * v[0] + m[1] * v[1] + m[2] * v[2],
+            m[3] * v[0] + m[4] * v[1] + m[5] * v[2],
+            m[6] * v[0] + m[7] * v[1] + m[8] * v[2]
         ];
     }
 
@@ -242,16 +244,16 @@ L.DomUtil.TRANSFORM_ORIGIN = L.DomUtil.testProp(
               x2s, y2s, x2d, y2d,
               x3s, y3s, x3d, y3d,
               x4s, y4s, x4d, y4d
-        ){
+        ) {
           var s = basisToPoints(x1s, y1s, x2s, y2s, x3s, y3s, x4s, y4s);
           var d = basisToPoints(x1d, y1d, x2d, y2d, x3d, y3d, x4d, y4d);
           return multmm(d, adj(s));
-        }, 
-        
+        },
+
         project: function(m, x, y) {
             var v = multmv(m, [x, y, 1]);
-            return [v[0]/v[2], v[1]/v[2]];
-        }, 
+            return [v[0] / v[2], v[1] / v[2]];
+        },
         adj: adj
-    }
-}();
+    };
+})();
